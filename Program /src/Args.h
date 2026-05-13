8b19ca86a6d23cd16a2783fafd7420633b2e006f
gnofori0815 <gnofori0815@my.msutexas.edu>
2026-05-12 21:18:03 -0500

    Create Args.h

---
+++ Program /src/Args.h
@@ -0,0 +1,27 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+#include <string>
+
+class Args {
+public:
+    std::string input;
+    std::string output;
+
+    bool grayscale = false;
+    bool blur = false;
+    bool flipH = false;
+    bool flipV = false;
+
+    bool use_brighten = false;
+    int brighten = 0;
+
+    bool use_rotate = false;
+    int rotate = 0;
+
+    static Args parse(int argc, char* argv[]);
+
+    void print() const;
+};
+
+#endif
